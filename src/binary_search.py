8331ed41baa06fdc49b1468e31f8ac8057a32aa5
Benedikt Seidl <benedikt.seidl@tum.de>
2018-10-29 00:18:04 +0100

    Add threshold to binary search

--- src/binary_search.py
+++ src/binary_search.py
@@ -6,10 +6,10 @@ def binary_search_list(lst, val):
     binary_search(lambda i: val - lst[i], 0, len(lst))
 
 
-def binary_search(fn, begin, end):
+def binary_search(fn, begin, end, threshold=0):
     current = (begin + end) / 2
 
-    while True:
+    while end - begin > threshold:
         val = fn(current)
 
         if val < 0:
@@ -17,6 +17,8 @@ def binary_search(fn, begin, end):
         elif val > 0:
             begin = current
         else:
-            return current
-        
+            break
+
         current = (begin + end) / 2
+
+    return current
