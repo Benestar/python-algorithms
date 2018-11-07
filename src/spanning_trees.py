a006ec7aa3bb974067c63075597862df51c0ddf4
Benedikt Seidl <benedikt.seidl@tum.de>
2018-11-07 13:56:49 +0100

    Refactor Prim's algorithm

--- src/spanning_trees.py
+++ src/spanning_trees.py
@@ -8,38 +8,34 @@ from union_find import UnionFind
 def kruskal(graph, weights):
     union_find = UnionFind(len(graph))
 
-    for (u, v) in sorted(weights, key=weights.get):
+    for u, v in sorted(weights, key=weights.get):
         if union_find.find(u) != union_find.find(v):
             union_find.union(u, v)
 
-            yield (u, v)
+            yield u, v
 
 
 def prim(graph, weights):
     INF = float('inf')
 
     values = [INF for _ in graph]
-    queue = [(INF, v) for v in graph]
-    pred = [None for _ in graph]
+    queue = [(INF, v, None) for v in graph]
     elements = set(graph)
 
     heapq.heapify(queue)
 
     while elements:
-        x, v = heapq.heappop(queue)
+        x, v, u = heapq.heappop(queue)
 
         try:
             elements.remove(v)
         except:
             continue
 
-        for u in graph[v]:
-            if u in elements and weights[v, u] < values[v]:
-                pred[u] = v
-
-                values[u] = weights[v, u]
-                heapq.heappush(queue, (values[u], u))
-
-    for u, v in enumerate(pred):
-        if v is not None:
+        if u is not None:
             yield u, v
+
+        for u in graph[v]:
+            if u in elements and weights[v, u] < values[u]:
+                values[u] = weights[v, u]
+                heapq.heappush(queue, (values[u], u, v))
